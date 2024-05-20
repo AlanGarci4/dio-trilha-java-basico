bfe83c5d98bb2f743a444e4b7172d5a7bd4972e6
AlanGarci4 <alangarciasantos@outlook.com>
2024-05-20 14:52:52 -0300

    Porjeto ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+       int numero = 1001-0;
+       String agencia;
+       String nomeCliente;
+       double saldo = 0.00;
+           
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+       
+       System.out.println("Digite a Agência");
+       agencia = scanner.next();
+
+       System.out.println("Digite Seu Nome");
+       nomeCliente = scanner.next();
+
+       System.out.println("Olá " + nomeCliente + ", " + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero +" e o seu saldo de " + saldo + ", já está disponível para movimentações e saques.");
+      
     }
 }
